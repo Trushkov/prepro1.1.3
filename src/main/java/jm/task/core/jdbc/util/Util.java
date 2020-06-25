08991e12c893f540997102b77ff576a2d99431e7
Nikolai Trushkov <tnv2511@mail.ru>
2020-06-25 20:14:49 +0300

    create getSessionFactory()

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,11 +1,19 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
 import java.lang.reflect.InvocationTargetException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
+
+    private static SessionFactory sessionFactory;
+
     public static Connection getConnection() throws SQLException {
         String url = "jdbc:mysql://localhost:3306/mydb1?serverTimezone=Europe/Moscow&useSSL=false";
         String userName = "root";
@@ -18,5 +26,20 @@ public class Util {
             e.printStackTrace();
         }
         return DriverManager.getConnection(url, userName, password);
+    }
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration().configure();
+                configuration.addAnnotatedClass(User.class);
+                StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
+                sessionFactory = configuration.buildSessionFactory(builder.build());
+
+            } catch (Exception e) {
+                System.out.println("Исключение!" + e);
+            }
+        }
+        return sessionFactory;
     }
 }
